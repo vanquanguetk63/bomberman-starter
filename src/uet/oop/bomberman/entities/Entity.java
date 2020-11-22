e45d722b1d313857f7dd2f1d2e80ae63c296f922
vanquanguetk63 <vanquang.3799@gmai.com>
2020-11-22 22:12:38 +0700

    update

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -1,27 +1,67 @@
 package uet.oop.bomberman.entities;
 
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import uet.oop.bomberman.graphics.IRender;
+import uet.oop.bomberman.graphics.Screen;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.level.Coordinates;
 
-public abstract class Entity {
-    protected int x;
-    protected int y;
-    protected Sprite sprite;
+/**
+ * Lớp đại diện cho tất cả thực thể trong game (Bomber, Enemy, Wall, Brick,...)
+ */
+public abstract class Entity implements IRender {
+
+	protected double _x, _y;
+	protected boolean _removed = false;
+	protected Sprite _sprite;
+
+	/**
+	 * Phương thức này được gọi liên tục trong vòng lặp game,
+	 * mục đích để xử lý sự kiện và cập nhật trạng thái Entity
+	 */
+	@Override
+	public abstract void update();
+
+	/**
+	 * Phương thức này được gọi liên tục trong vòng lặp game,
+	 * mục đích để cập nhật hình ảnh của entity theo trạng thái
+	 */
+	@Override
+	public abstract void render(Screen screen);
+	
+	public void remove() {
+		_removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return _removed;
+	}
+	
+	public Sprite getSprite() {
+		return _sprite;
+	}
+
+	/**
+	 * Phương thức này được gọi để xử lý khi hai entity va chạm vào nhau
+	 * @param e
+	 * @return
+	 */
+	public abstract boolean collide(Entity e);
+	
+	public double getX() {
+		return _x;
+	}
+	
+	public double getY() {
+		return _y;
+	}
+	
+	public int getXTile() {
+		return Coordinates.pixelToTile(_x + _sprite.SIZE / 2);
+	}
+	
+	public int getYTile() {
+		return Coordinates.pixelToTile(_y - _sprite.SIZE / 2);
+	}
 
 
-    public void render(GraphicsContext gc) {
-//        SnapshotParameters params = new SnapshotParameters();
-//        params.setFill(Color.TRANSPARENT);
-//
-//        ImageView iv = new ImageView(this.sprite.getFxImage());
-//        Image base = iv.snapshot(params, null);
-
-        gc.drawImage(this.sprite.getFxImage(), x , y);
-    }
-
-    public abstract void update();
 }
